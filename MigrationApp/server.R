110dd985d25537b4c10e833d5fe8d6682dc4dbce
William Conyers <williamconyers@Williams-MacBook-2.local>
2020-12-17 14:15:23 -0800

    Addition of maps, not quite pretty yet.

--- MigrationApp/server.R
+++ MigrationApp/server.R
@@ -3,6 +3,10 @@
 # 
 
 library(shiny)
+library(plotly)
+library(tidyverse)
+library(maps)
+library(mapproj)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output,session) {
@@ -64,15 +68,58 @@ shinyServer(function(input, output,session) {
                    )
     })
     
+    #read in data for world map
+    world <- map_data("world")
+    
+    #originMap is a world chloroplast map showing number of refugees coming from each country.
+    output$originMap <- renderPlot({
 
-    output$distPlot <- renderPlot({
+        #make data set for Origin map
+        dataOrigin = data %>%
+            group_by(Origin, Year) %>%
+            summarise(Generated = sum(Refugees, na.rm=TRUE)) %>%
+            filter(Year >= input$years2[1],
+                   Year <= input$years2[2])
+        mapOrigin <- left_join(world,dataOrigin,by=c("region"="Origin"))
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        #make map
+        ggplot(mapOrigin, aes(x=long, y=lat, group=group, fill=Generated)) +
+            geom_polygon(color = "white", size=0.2) +
+            theme_void() +
+            scale_fill_gradient(low="white",
+                                high="firebrick",
+                                trans = "log") +
+            labs(title="Refugees Produced by Country",
+                 subtitle = "",
+                 fill="Number of \nRefugees") +
+            theme(plot.title = element_text(hjust = 0.5, size = 15),
+                  legend.position = c(0.15, 0.35))
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    
+    #residenceMap is a world chloroplast map showing number of refugees accepted by each country.
+    output$residenceMap <- renderPlot({
+
+        #make data set for Origin map
+        dataResidence = data %>%
+            group_by(Residence, Year) %>%
+            summarise(Accepted = sum(Refugees, na.rm=TRUE)) %>%
+            filter(Year >= input$years2[1],
+                   Year <= input$years2[2])
+        mapResidence <- left_join(world,dataResidence,by=c("region"="Residence"))
+
+        #make map
+        ggplot(mapResidence, aes(x=long, y=lat, group=group, fill=Accepted)) +
+            geom_polygon(color = "white", size=0.2) +
+            theme_void() +
+            scale_fill_gradient(low="white",
+                                high="blue",
+                                trans = "log") +
+            labs(title="Refugees Accepted by Country",
+                 subtitle = "",
+                 fill="Number of \nRefugees") +
+            theme(plot.title = element_text(hjust = 0.5, size = 15),
+                  legend.position = c(0.15, 0.35))
 
     })
 
--- MigrationApp/ui.R
+++ MigrationApp/ui.R
@@ -1,13 +1,9 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# User Interface for Refugee Migration Dashboard
+# 
 
 library(shiny)
+library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -20,7 +16,9 @@ shinyUI(fluidPage(
         tabPanel( "Profiler",
                   fluid = TRUE,
                   sidebarLayout(
+                      
                       sidebarPanel(
+                          
                           sliderInput("years",
                                       "Timeperiod:",
                                       min = 1975,
@@ -28,28 +26,40 @@ shinyUI(fluidPage(
                                       sep = "",
                                       value = c(2000,2016)
                                       ),
-                          selectInput("Country",
-                                      "Select Country:",
-                                      choices = NULL),
+                          
+                          hr(),
+                          
+                          checkboxInput("countryyes", "View an Individual Country?"),
+                          
+                          conditionalPanel(condition='input.countryyes',
+                                           selectInput("country", 
+                                                       "Select Country:", 
+                                                       choices=NULL)
+                                           )
+                          
                           ),
+                      
                       mainPanel(
                           plotlyOutput("yearPlot")
                           )
                       )
         ),
         
-        tabPanel( "Second Tab",
+        tabPanel( "Movement",
                   fluid = TRUE,
                   sidebarLayout(
                       sidebarPanel(
-                          sliderInput("bins",
-                                      "NUMBER of bins:",
-                                      min = 1,
-                                      max = 50,
-                                      value = 30)
+                          sliderInput("years2",
+                                      "Timeperiod:",
+                                      min = 1975,
+                                      max = 2016,
+                                      sep = "",
+                                      value = c(2000,2016)
+                          ),
                       ),
                       mainPanel(
-                          plotOutput("distPlot")
+                          plotOutput("originMap"),
+                          plotOutput("residenceMap")
                       )
                   )
         )
